a4d7a152c6ed4b162b56f3d2512cb492309e09e2
QuinnYap <b2029qcyap@pshs.edu.ph>
2026-01-19 08:45:50 +0800

    fixed issues

--- public/q3/q32gliyap.js
+++ public/q3/q32gliyap.js
@@ -1,12 +1,47 @@
 const form = document.getElementById("club");
 
-//localstorage saving
+form.addEventListener("submit", function (e){
+    e.preventDefault();
+    if(confirm("You want to save your work?")){
+        const data = new FormData(form);
+        const infoObj = Object.fromEntries(data.entries());
+    }
+});
 
-form.addEventListener("reset", function(e){ 
-    if(!confirm("Are you sure you want to clear your data?")){
+form.addEventListener("reset", function (e){ 
+    if(!confirm("You want to clear your data?")){
         e.preventDefault();
     }
 });
+
+function showDesc(){
+    let club = document.getElementById("clubSelect").value;
+    let clubDesc;
+    switch(club){
+        case "artcl":
+            clubDesc = "The arts & crafts club is a place where you can let your creativity flow.";
+            break;
+        case "dancecl":
+            clubDesc = "The dance club is a place where you can show off your dance skills.";
+            break;
+        case "singcl":
+            clubDesc = "The choir club is a place where you can realize your true singing talent.";
+            break;
+        case "musiccl":
+            clubDesc = "The music club is a place where you can learn how to use instruments, or show people your talent in music.";
+            break;
+        case "theatrecl":
+            clubDesc = "The theatre club is a place where you can reveal your true dramatic side.";
+            break;
+        case "ultfrisbeecl":
+            clubDesc = "The ultimate frisbee club is a place where you can exercise your frisbee skill or participate in tournaments.";
+            break;
+        default:
+            clubDesc = "Club Description Here"
+    }
+    document.getElementById("clubText").innerHTML = "Club Description: " + clubDesc
+  
+}
 
 function changeColor(dInput){
     dInput.style.backgroundColor = "#dec1db";
@@ -15,3 +50,4 @@ function changeColor(dInput){
 function resetColor(dInput){
     dInput.style.backgroundColor = "white";
 }
+
